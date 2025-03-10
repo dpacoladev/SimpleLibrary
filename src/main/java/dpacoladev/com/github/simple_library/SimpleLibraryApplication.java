f52f18106f1159e06d30e5179412058d9ca7b7d5
Dalton P. Franco <dpacola.dev@gmail.com>
2025-03-10 18:12:43 -0300

    First Commit

---
+++ src/main/java/dpacoladev/com/github/simple_library/SimpleLibraryApplication.java
@@ -0,0 +1,11 @@
+package dpacoladev.com.github.simple_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLibraryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLibraryApplication.class, args);
+	}
+}
---
+++ src/test/java/dpacoladev/com/github/simple_library/SimpleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dpacoladev.com.github.simple_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
